b4bd5762a32ac5d5f910b2304213cffd8508ceb7
Varghese Bobus <varghesebobus@gmail.com>
2021-08-11 01:02:03 -0700

    initial commit side scrolling game

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+
+    private float speed = 20.0f;
+    private PlayerController playerControllerScript;
+    private float leftBound;
+    public float diffIncreaseTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        diffIncreaseTime = Time.time + 3.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (transform.position.x < -10 && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (Time.time > diffIncreaseTime)
+        {
+            speed += 3.0f;
+            diffIncreaseTime += 3.0f;
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] obstaclePrefab;
+    Vector3 spawnPos = new Vector3(35, 0, 0);
+    private float startDelay = 2.0f;
+    private float repeatRate = 2.0f;
+    private PlayerController playerControllerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnObstacle()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            int obstacleIndex = Random.Range(0, obstaclePrefab.Length);
+            Instantiate(obstaclePrefab[obstacleIndex], spawnPos, obstaclePrefab[obstacleIndex].transform.rotation);
+        }
+
+    }
+}
